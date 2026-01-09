c4c69a2d02e58ad43592257e07b0f0c46dd97bcf
Bhavesh-Sapar <bhaveshsapar07@gmail.com>
2026-01-09 12:16:35 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,33 +1,26 @@
 import os
-
 import streamlit as st
 
 from rag_utility import process_document_to_chroma_db, answer_question
 
-
-# set the working directory
-working_dir = os.path.dirname(os.path.abspath((__file__)))
+working_dir = os.path.dirname(os.path.abspath(__file__))
 
 st.title("🦙 Llama-3.3-70B - Document RAG")
 
-# file uploader widget
 uploaded_file = st.file_uploader("Upload a PDF file", type=["pdf"])
 
 if uploaded_file is not None:
-    # define save path
-    save_path = os.path.join(working_dir, uploaded_file.name)
-    #  save the file
+    save_path = f"{working_dir}/{uploaded_file.name}"
+
     with open(save_path, "wb") as f:
         f.write(uploaded_file.getbuffer())
 
-    process_document = process_document_to_chroma_db(uploaded_file.name)
-    st.info("Document Processed Successfully")
+    process_document_to_chroma_db(uploaded_file.name)
+    st.success("Document Processed Successfully")
 
-# text widget to get user input
 user_question = st.text_area("Ask your question about the document")
 
-if st.button("Answer"):
-
+if st.button("Answer") and user_question.strip():
     answer = answer_question(user_question)
 
     st.markdown("### Llama-3.3-70B Response")
